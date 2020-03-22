8aaa47eb91a4baddbdf49c4b05e6193fcbb91137
tekkenthuuug <sa.soto317@gmail.com>
2020-03-22 20:18:27 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# SmartBrain
+Project for Udemy course by <a href="https://github.com/aneagoie">Andrei Neagoie</a>
+
+1. Clone this repo
+2. Run `npm install`
+3. Run `npm start`
---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './FaceRecognition.css';
+
+const FaceRecognition = ({ imageUrl, boxes }) => {
+  return (
+    <div className="center ma">
+      <div className="absolute mt2">
+        <img id="inputimage" alt="" src={imageUrl} width="500px" heigh="auto" />
+        {boxes
+          ? boxes.map((box, index) => {
+              return (
+                <div
+                  key={index}
+                  className="bounding-box"
+                  style={{ top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol }}
+                ></div>
+              );
+            })
+          : null}
+      </div>
+    </div>
+  );
+};
+
+export default FaceRecognition;
